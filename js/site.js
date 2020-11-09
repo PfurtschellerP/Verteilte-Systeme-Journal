3bc593d506d7e2a9cc54b1a74623cb12eea6f7b4
Patrick Pfurtscheller <patrick@pfurtscheller.org>
2020-11-09 17:36:26 +0100

    added to top button

---
+++ js/site.js
@@ -0,0 +1,17 @@
+// Scroll up Button
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("goToTopButton").style.display = "block";
+  } else {
+    document.getElementById("goToTopButton").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
