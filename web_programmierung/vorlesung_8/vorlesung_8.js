ac72c35b6b67c9d407065b4c4dd7b405ef394967
Patrick Pfurtscheller <patrick@pfurtscheller.org>
2020-12-10 11:45:48 +0100

    Vorlesung 8 abgeschlossen

--- web_programmierung/vorlesung_8/vorlesung_8.js
+++ web_programmierung/vorlesung_8/vorlesung_8.js
@@ -18,4 +18,23 @@ $(document).ready(function( ) {
             $(this).next().show();
         }
     })
+
+    // Event Objekt
+    $("#eventButton").click(function(evt){
+        var xPos = evt.pageX;
+        var yPos = evt.pageY;
+        alert("X: " + xPos + " Y: " + yPos);
+    })
+
+    // Event Verhalten überschreiben
+
+    $("#eventVerhalten").click(function(evt){
+        if ($("#eventVerhaltenCheck").is(":checked")){
+            alert("Mutter: Wir haben noch Google zu Hause");
+            alert("Google zu Hause:");
+            window.open("https://bing.com");
+            evt.preventDefault();
+        }
+    })
 });
+
